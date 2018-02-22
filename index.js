402168cec321f60d0953dca81488c62bbdb99390
Nathan127 <nathandestafeno@gmail.com>
2018-02-22 15:57:10 -0800

    Removed excess code and console outputs in preperation for qualtrics integration.

--- index.js
+++ index.js
@@ -15,13 +15,6 @@ var blueberryBool = 0
 
 var interval = 1; //# of milliseconds per interval
 
-document.getElementById("madagascarPic_count").innerHTML = madagascarPic_count; //***
-document.getElementById("honeyRoastedPic_count").innerHTML = honeyRoastedPic_count;//***
-document.getElementById("almondPic_count").innerHTML = almondPic_count;//***
-document.getElementById("blueberryPic_count").innerHTML = blueberryPic_count;//***
-
-document.getElementById("madagascarPic_time").innerHTML = madagascarPic_count;//***
-
 document.getElementById("madagascarPic").addEventListener("mousedown", mouseDownmadagascarPic);
 document.getElementById("madagascarPic").addEventListener("mouseup", mouseUpmadagascarPic);
 
@@ -41,9 +34,7 @@ setInterval(blueberryTimer, interval);
 
 function madagascarTimer() {
   if (madagascarBool == 1)
-  {
     madagascarPic_time++;
-  }
 }
 
 function honeyRoastedTimer() {
@@ -63,44 +54,36 @@ function blueberryTimer() {
 
 function mouseDownmadagascarPic() {
   madagascarPic_count++;
-  document.getElementById("madagascarPic_count").innerHTML = madagascarPic_count;//***
   madagascarBool = 1;
 }
 
 function mouseUpmadagascarPic() {
   madagascarBool = 0
-  document.getElementById("madagascarPic_time").innerHTML = madagascarPic_time;//***
 }
 
 function mouseDownhoneyRoastedPic() {
   honeyRoastedPic_count++;
-  document.getElementById("honeyRoastedPic_count").innerHTML = honeyRoastedPic_count;//***
   honeyRoastedBool = 1;
 }
 
 function mouseUphoneyRoastedPic() {
   honeyRoastedBool = 0
-  document.getElementById("honeyRoastedPic_time").innerHTML = honeyRoastedPic_time;//***
 }
 
 function mouseDownalmondPic() {
   almondPic_count++;
-  document.getElementById("almondPic_count").innerHTML = almondPic_count;//***
   almondBool = 1;
 }
 
 function mouseUpalmondPic() {
   almondBool = 0
-  document.getElementById("almondPic_time").innerHTML = almondPic_time;//***
 }
 
 function mouseDownblueberryPic() {
   blueberryPic_count++;
-  document.getElementById("blueberryPic_count").innerHTML = blueberryPic_count;//***
   blueberryBool = 1;
 }
 
 function mouseUpblueberryPic() {
   blueberryBool = 0
-  document.getElementById("blueberryPic_time").innerHTML = blueberryPic_time;//***
 }
