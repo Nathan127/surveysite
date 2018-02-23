d923d7ea12e970edb8ad018b254a018b3351bc40
Nathan127 <nathandestafeno@gmail.com>
2018-02-23 10:14:23 -0800

    Added code to reflect the changes we had to make to get it to work with qualtrics. Changed Madagascar to Chocolate. Changed time interval from 1ms to 20ms

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
-var madagascarPic_count = 0;
+Qualtrics.SurveyEngine.addOnload(function()
+{
+	/*Place your JavaScript here to run when the page loads*/
+var chocolatePic_count = 0;
 var honeyRoastedPic_count = 0;
 var almondPic_count = 0;
 var blueberryPic_count = 0;
 
-var madagascarPic_time = 0;
+var chocolatePic_time = 0;
 var honeyRoastedPic_time = 0;
 var almondPic_time = 0;
 var blueberryPic_time = 0;
 
-var madagascarBool = 0
+var chocolateBool = 0
 var honeyRoastedBool = 0
 var almondBool = 0
 var blueberryBool = 0
 
-var interval = 1; //# of milliseconds per interval
+var interval = 20; //# of milliseconds per interval
 
-document.getElementById("madagascarPic").addEventListener("mousedown", mouseDownmadagascarPic);
-document.getElementById("madagascarPic").addEventListener("mouseup", mouseUpmadagascarPic);
+document.getElementById("chocolatePic_count").innerHTML = chocolatePic_count; //***
+document.getElementById("honeyRoastedPic_count").innerHTML = honeyRoastedPic_count;//***
+document.getElementById("almondPic_count").innerHTML = almondPic_count;//***
+document.getElementById("blueberryPic_count").innerHTML = blueberryPic_count;//***
+
+document.getElementById("chocolatePic_time").innerHTML = chocolatePic_count;//***
+
+document.getElementById("chocolatePic").addEventListener("mousedown", mouseDownchocolatePic);
+document.getElementById("chocolatePic").addEventListener("mouseup", mouseUpchocolatePic);
 
 document.getElementById("honeyRoastedPic").addEventListener("mousedown", mouseDownhoneyRoastedPic);
 document.getElementById("honeyRoastedPic").addEventListener("mouseup", mouseUphoneyRoastedPic);
@@ -27,14 +37,16 @@ document.getElementById("almondPic").addEventListener("mouseup", mouseUpalmondPi
 document.getElementById("blueberryPic").addEventListener("mousedown", mouseDownblueberryPic);
 document.getElementById("blueberryPic").addEventListener("mouseup", mouseUpblueberryPic);
 
-setInterval(madagascarTimer, interval);
+setInterval(chocolateTimer, interval);
 setInterval(honeyRoastedTimer, interval);
 setInterval(almondTimer, interval);
 setInterval(blueberryTimer, interval);
 
-function madagascarTimer() {
-  if (madagascarBool == 1)
-    madagascarPic_time++;
+function chocolateTimer() {
+  if (chocolateBool == 1)
+  {
+    chocolatePic_time++;
+  }
 }
 
 function honeyRoastedTimer() {
@@ -52,38 +64,72 @@ function blueberryTimer() {
     blueberryPic_time++;
 }
 
-function mouseDownmadagascarPic() {
-  madagascarPic_count++;
-  madagascarBool = 1;
+function mouseDownchocolatePic() {
+  chocolatePic_count++;
+  document.getElementById("chocolatePic_count").innerHTML = chocolatePic_count;//***
+  chocolateBool = 1;
 }
 
-function mouseUpmadagascarPic() {
-  madagascarBool = 0
+function mouseUpchocolatePic() {
+  chocolateBool = 0
+  document.getElementById("chocolatePic_time").innerHTML = chocolatePic_time;//***
 }
 
 function mouseDownhoneyRoastedPic() {
   honeyRoastedPic_count++;
+  document.getElementById("honeyRoastedPic_count").innerHTML = honeyRoastedPic_count;//***
   honeyRoastedBool = 1;
 }
 
 function mouseUphoneyRoastedPic() {
   honeyRoastedBool = 0
+  document.getElementById("honeyRoastedPic_time").innerHTML = honeyRoastedPic_time;//***
 }
 
 function mouseDownalmondPic() {
   almondPic_count++;
+  document.getElementById("almondPic_count").innerHTML = almondPic_count;//***
   almondBool = 1;
 }
 
 function mouseUpalmondPic() {
   almondBool = 0
+  document.getElementById("almondPic_time").innerHTML = almondPic_time;//***
 }
 
 function mouseDownblueberryPic() {
   blueberryPic_count++;
+  document.getElementById("blueberryPic_count").innerHTML = blueberryPic_count;//***
   blueberryBool = 1;
 }
 
 function mouseUpblueberryPic() {
   blueberryBool = 0
+  document.getElementById("blueberryPic_time").innerHTML = blueberryPic_time;//***
 }
+
+	Qualtrics.SurveyEngine.addOnPageSubmit(function(type)
+{
+	if(type == "next")
+	{
+		Qualtrics.SurveyEngine.setEmbeddedData("chocolatePic_count", chocolatePic_count);
+  	Qualtrics.SurveyEngine.setEmbeddedData("honeyRoastedPic_count", honeyRoastedPic_count);
+  	Qualtrics.SurveyEngine.setEmbeddedData("almondPic_count", almondPic_count);
+		Qualtrics.SurveyEngine.setEmbeddedData("blueberryPic_count", blueberryPic_count);
+	  Qualtrics.SurveyEngine.setEmbeddedData("chocolatePic_time", chocolatePic_time);
+  	Qualtrics.SurveyEngine.setEmbeddedData("honeyRoastedPic_time", honeyRoastedPic_time);
+  	Qualtrics.SurveyEngine.setEmbeddedData("almondPic_time", almondPic_time);
+		Qualtrics.SurveyEngine.setEmbeddedData("blueberryPic_time", blueberryPic_time);
+	}
+});
+});
+
+Qualtrics.SurveyEngine.addOnReady(function()
+{
+	/*Place your JavaScript here to run when the page is fully displayed*/
+});
+
+Qualtrics.SurveyEngine.addOnUnload(function()
+{
+	/*Place your JavaScript here to run when the page is unloaded*/
+});
