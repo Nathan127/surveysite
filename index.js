6d6fbdaf62fd6b962b79eafe82935bd465369636
DrNathan100 <drnathan100@gmail.com>
2018-02-24 19:04:30 -0800

    Fixed the randomize function to record data with qualtrics, hide displayed HTML data, and fixed a bug causing the data to not match with their picture

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 Qualtrics.SurveyEngine.addOnload(function()
 {
+	/*Place your JavaScript here to run when the page loads*/
+});
+
+Qualtrics.SurveyEngine.addOnReady(function()
+{
+	/*Place your JavaScript here to run when the page is fully displayed*/
+	
 //Initialize Qualtrics counters
 var chocolatePic_count = 0;
 var honeyRoastedPic_count = 0;
@@ -33,16 +40,16 @@ var quad4Bool = 0
 var interval = 20; //# of milliseconds per interval
 
 //Declare URL for letting go of click
-var chocolateURL_up = "http://tamaskan-dog.com/Galleries/Puppy%20Gallery/Image%209.jpg";
-var honeyRoastedURL_up = "http://images4.fanpop.com/image/photos/16000000/Cute-Kitten-kittens-16096566-1280-800.jpg";
-var almondURL_up = "https://78.media.tumblr.com/8ecb1bf9e35eacf4565089cc9e1d7202/tumblr_neop8e3xjr1u01oxko1_1280.jpg";
-var blueberryURL_up = "https://oregonstate.qualtrics.com/CP/Graphic.php?IM=IM_bgvIrINID92Taeh";
+var chocolateURL_up = "https://oregonstate.qualtrics.com/CP/Graphic.php?IM=IM_6sSViPp5Njze9ut";
+var honeyRoastedURL_up = "https://oregonstate.qualtrics.com/CP/Graphic.php?IM=IM_eWHTYyUe9Z0bOn3";
+var almondURL_up = "https://oregonstate.qualtrics.com/CP/Graphic.php?IM=IM_0eu8FOzTokZWnOJ";
+var blueberryURL_up = "https://oregonstate.qualtrics.com/CP/Graphic.php?IM=IM_5AdNUQ0lGj2C5cp";
 
 //Declare URLS for clicking
-var chocolateURL_down = "http://www.tamaskan-dog.com/Galleries/Puppy%20Gallery/Image%208.jpg";
-var honeyRoastedURL_down = "http://images4.fanpop.com/image/photos/16100000/Cute-Kitten-kittens-16123151-1280-800.jpg";
-var almondURL_down = "http://weknowyourdreams.com/single/rabbit/rabbit-02";
-var blueberryURL_down = "https://oregonstate.qualtrics.com/CP/Graphic.php?IM=IM_bgvIrINID92Taeh";
+var chocolateURL_down = "https://oregonstate.qualtrics.com/CP/Graphic.php?IM=IM_3w1PdWmf5oRzYMt";
+var honeyRoastedURL_down = "https://oregonstate.qualtrics.com/CP/Graphic.php?IM=IM_3w1PdWmf5oRzYMt";
+var almondURL_down = "https://oregonstate.qualtrics.com/CP/Graphic.php?IM=IM_3w1PdWmf5oRzYMt";
+var blueberryURL_down = "https://oregonstate.qualtrics.com/CP/Graphic.php?IM=IM_3w1PdWmf5oRzYMt";
 
 //Declare URLS for img sources
 var chocolateURL_src = chocolateURL_up;
@@ -138,47 +145,57 @@ function mouseDownQuad1() {
   quad1_count++;
   quad1Bool = 1;
   document.quadOne.src = mouseDownArray[randArray[0]]
+  pageUpdate();
 }
 
 //When quadrant one is released, stop the timer and change the image again.
 function mouseUpQuad1() {
   quad1Bool = 0
   document.quadOne.src = mouseUpArray[randArray[0]]
+	pageUpdate();
 }
 
 function mouseDownQuad2() {
   quad2_count++;
   quad2Bool = 1;
   document.quadTwo.src = mouseDownArray[randArray[1]]
+  pageUpdate();
 }
 
 function mouseUpQuad2() {
   quad2Bool = 0
   document.quadTwo.src = mouseUpArray[randArray[1]]
+  pageUpdate();
 }
 
 function mouseDownQuad3() {
   quad3_count++;
   quad3Bool = 1;
   document.quadThree.src = mouseDownArray[randArray[2]]
+  pageUpdate();
 }
 
 function mouseUpQuad3() {
   quad3Bool = 0
   document.quadThree.src = mouseUpArray[randArray[2]]
+  pageUpdate();
 }
 
 function mouseDownQuad4() {
   quad4_count++;
   quad4Bool = 1;
   document.quadFour.src = mouseDownArray[randArray[3]]
+  pageUpdate();
 }
 
 function mouseUpQuad4() {
   quad4Bool = 0
   document.quadFour.src = mouseUpArray[randArray[3]]
+  pageUpdate();
 }
 
+function pageUpdate(){
+console.log("update fired");
 //Put timer and counter values in arrays for easy access
 var timeArray = [quad1_time, quad2_time, quad3_time, quad4_time]
 var countArray = [quad1_count, quad2_count, quad3_count, quad4_count]
@@ -194,6 +211,16 @@ honeyRoastedPic_time = timeArray[randArray[1]];
 almondPic_time = timeArray[randArray[2]];
 blueberryPic_time = timeArray[randArray[3]];
 
+document.getElementById("chocolatePic_count").innerHTML = chocolatePic_count;
+document.getElementById("chocolatePic_time").innerHTML = chocolatePic_time;
+document.getElementById("honeyRoastedPic_count").innerHTML = honeyRoastedPic_count;
+document.getElementById("honeyRoastedPic_time").innerHTML = honeyRoastedPic_time;
+document.getElementById("almondPic_count").innerHTML = almondPic_count;
+document.getElementById("almondPic_time").innerHTML = almondPic_time;
+document.getElementById("blueberryPic_count").innerHTML = blueberryPic_count;
+document.getElementById("blueberryPic_time").innerHTML = blueberryPic_time;
+}
+
 Qualtrics.SurveyEngine.addOnPageSubmit(function(type)
 {
 	if(type == "next")
@@ -206,17 +233,78 @@ Qualtrics.SurveyEngine.addOnPageSubmit(function(type)
 		Qualtrics.SurveyEngine.setEmbeddedData("honeyRoastedPic_time", honeyRoastedPic_time);
 		Qualtrics.SurveyEngine.setEmbeddedData("almondPic_time", almondPic_time);
 		Qualtrics.SurveyEngine.setEmbeddedData("blueberryPic_time", blueberryPic_time);
+		
+		//Put timer and counter values in arrays for easy access
+	var timeArray = [quad1_time, quad2_time, quad3_time, quad4_time]
+	var countArray = [quad1_count, quad2_count, quad3_count, quad4_count]
+
+	//Take the quadrant data and put them in their appropriate qualtrics locations
+	chocolatePic_count = countArray[randArray[0]];
+	honeyRoastedPic_count = countArray[randArray[1]];
+	almondPic_count = countArray[randArray[2]];
+	blueberryPic_count = countArray[randArray[3]];
+
+	chocolatePic_time = timeArray[randArray[0]];
+	honeyRoastedPic_time = timeArray[randArray[1]];
+	almondPic_time = timeArray[randArray[2]];
+	blueberryPic_time = timeArray[randArray[3]];
 	}
+	
+	//Put timer and counter values in arrays for easy access
+	var timeArray = [quad1_time, quad2_time, quad3_time, quad4_time]
+	var countArray = [quad1_count, quad2_count, quad3_count, quad4_count]
+
+	//Take the quadrant data and put them in their appropriate qualtrics locations
+	chocolatePic_count = countArray[randArray[0]];
+	honeyRoastedPic_count = countArray[randArray[1]];
+	almondPic_count = countArray[randArray[2]];
+	blueberryPic_count = countArray[randArray[3]];
+
+	chocolatePic_time = timeArray[randArray[0]];
+	honeyRoastedPic_time = timeArray[randArray[1]];
+	almondPic_time = timeArray[randArray[2]];
+	blueberryPic_time = timeArray[randArray[3]];
 });
 
-});
-
-Qualtrics.SurveyEngine.addOnReady(function()
-{
-	/*Place your JavaScript here to run when the page is fully displayed*/
 });
 
 Qualtrics.SurveyEngine.addOnUnload(function()
 {
 	/*Place your JavaScript here to run when the page is unloaded*/
+	
+	//Put timer and counter values in arrays for easy access
+	var timeArray = [quad1_time, quad2_time, quad3_time, quad4_time]
+	var countArray = [quad1_count, quad2_count, quad3_count, quad4_count]
+
+	//Shuffle array to match other shuffled data
+	countArray[randArray[0]] = quad1_count;
+	countArray[randArray[1]] = quad2_count;
+	countArray[randArray[2]] = quad3_count;
+	countArray[randArray[3]] = quad4_count;
+
+	timeArray[randArray[0]] = quad1_time;
+	timeArray[randArray[1]] = quad2_time;
+	timeArray[randArray[2]] = quad3_time;
+	timeArray[randArray[3]] = quad4_time;
+
+	//Take the quadrant data and put them in their appropriate qualtrics locations
+	chocolatePic_count = countArray[0];
+	honeyRoastedPic_count = countArray[1];
+	almondPic_count = countArray[2];
+	blueberryPic_count = countArray[3];
+
+	chocolatePic_time = timeArray[0];
+	honeyRoastedPic_time = timeArray[1];
+	almondPic_time = timeArray[2];
+	blueberryPic_time = timeArray[3];
+
+	Qualtrics.SurveyEngine.setEmbeddedData("chocolatePic_count", chocolatePic_count);
+		Qualtrics.SurveyEngine.setEmbeddedData("honeyRoastedPic_count", honeyRoastedPic_count);
+		Qualtrics.SurveyEngine.setEmbeddedData("almondPic_count", almondPic_count);
+		Qualtrics.SurveyEngine.setEmbeddedData("blueberryPic_count", blueberryPic_count);
+		Qualtrics.SurveyEngine.setEmbeddedData("chocolatePic_time", chocolatePic_time);
+		Qualtrics.SurveyEngine.setEmbeddedData("honeyRoastedPic_time", honeyRoastedPic_time);
+		Qualtrics.SurveyEngine.setEmbeddedData("almondPic_time", almondPic_time);
+		Qualtrics.SurveyEngine.setEmbeddedData("blueberryPic_time", blueberryPic_time);
+	
 });
